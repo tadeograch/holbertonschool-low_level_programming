253326343a057b7d789e444672e96c8b915ee9db
Tadeo Grach <tade.g.17@gmail.com>
2021-01-15 03:53:57 +0000

    fixing task 4

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -14,6 +14,8 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int index;
 	hash_node_t *aux_node;
 
+	if (!ht | !key)
+		return (NULL);
 	index = key_index((unsigned const char*)(char *)key, ht->size);
 	aux_node = (ht->array)[index];
 	while (aux_node != NULL)
