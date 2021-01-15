b04cd421ae7229490c27d3a6d060e3fdd3683993
Tadeo Grach <tade.g.17@gmail.com>
2021-01-15 04:20:14 +0000

    fixing task 5

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -14,11 +14,8 @@ void hash_table_print(const hash_table_t *ht)
 	unsigned long int i;
 	int flag = 0;
 
-	if (!ht)
-	{
-		printf("{}\n");
+	if (!ht->array)
 		return;
-	}
 	printf("{");
 	for (i = 0; i < ht->size; i++)
 	{
